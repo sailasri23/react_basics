d217d0e1d297402ceb7a034e2a65538a0682b710
sailasri23 <tungasailasri12@gmail.com>
2024-02-18 17:11:39 +0530

    components_smaller

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-//components cant just use data stored in other components
-//we can pass data by adding a attribute
-//props are nothing but attributes in html
-//we can also pass hard coded data to props like title = toilet but props need to be reusable
-
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2020, 2, 28),
-      EOL:'Mumbai'
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "e2",
-      title: "Car Insurance",
-      amount: 2.67,
-      date: new Date(2021, 9, 28),
-      EOL:'chennai'
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
-    { id: "e3", title: "New Desk", amount: 29, date: new Date(2021, 10, 28), EOL:'chennai' },
-    { id: "e4", title: "Book", amount: 299, date: new Date(2021, 9, 28), EOL:'kerela' },
   ];
+
   return (
     <div>
-      <h2>Let's get started</h2>
+      <h2>Let's get started!</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-        E={expenses[0].EOL}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-        E={expenses[1].EOL}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-        E={expenses[2].EOL}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-        E={expenses[3].EOL}
-
-      ></ExpenseItem>
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
-import "./ExpenseItem.css";
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US',{month:'long'})
-  const day = props.date.toLocaleString('en-US',{day:'2-digit'})
-  const year = props.date.getFullYear()
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      <div className="expense-item__description">
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <h3>{props.E}</h3>
-        <div className="expense-item__price">Rs{props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
 }
+
 export default ExpenseItem;
