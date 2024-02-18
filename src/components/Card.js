7177848822ab3e3a120ca1cf9ccafea471aade1c
sailasri23 <tungasailasri12@gmail.com>
2024-02-18 19:35:25 +0530

    added comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,12 @@
+/*
+In short, a card in React is a reusable UI component used to display information or content in a visually 
+appealing and organized manner. It typically consists of HTML elements styled with CSS to create a distinct 
+container, often containing text, images, buttons, or other interactive elements. Cards are commonly used in web 
+development to present various types of content such as product details, user profiles, articles, and more.
+
+//here we bring some css into a duplicate wrapeer component
+ */
+
 import './Card.css';
 
 function Card(props) {
