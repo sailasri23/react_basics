56623e199139aa2b9d2e00066374a5976336ad94
sailasri23 <tungasailasri12@gmail.com>
2024-02-18 16:40:49 +0530

    used_props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,17 @@
-
-import './ExpenseItem.css';
-
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+//for components reusabilty ,we use props
+//here like js, we need to pass one paramater which is object holds all received properties as attributes called props(properties)
+//props are another way passing data from component a to b.
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h3>{props.E}</h3>
+        <div className="expense-item__price">Rs{props.amount}</div>
       </div>
     </div>
   );
 }
-
 export default ExpenseItem;
-
-//to output dynamic data,react have that syntax curly braces which are dynamic place holders
-//in curly braces we can run basic expression
